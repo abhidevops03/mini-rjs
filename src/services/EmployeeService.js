ee501d0c9fdf9d5309947b68b7ff02bf45d7f7e0
Jenkins <jenkins@jenkins.us-west1-a.c.tokyo-dispatch-434612-c7.internal>
2024-10-21 13:25:03 +0000

    added all reactfiles

---
+++ src/services/EmployeeService.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const EMPLOYEE_API_BASE_URL = "http://k8s-default-devops19-dc457ef2d4-8013d4dd7465545e.elb.us-west-2.amazonaws.com:8080/api/v1/employees"
+
+class EmployeeService {
+
+    getEmployees(){
+        return axios.get(EMPLOYEE_API_BASE_URL);
+    }
+
+    createEmployee(employee){
+        return axios.post(EMPLOYEE_API_BASE_URL, employee);
+    }
+
+    getEmployeeById(employeeId){
+        return axios.get(EMPLOYEE_API_BASE_URL + '/' + employeeId);
+    }
+
+    updateEmployee(employee, employeeId){
+        return axios.put(EMPLOYEE_API_BASE_URL + '/' + employeeId, employee);
+    }
+
+    deleteEmployee(employeeId){
+        return axios.delete(EMPLOYEE_API_BASE_URL + '/' + employeeId);
+    }
+}
+
+export default new EmployeeService()
